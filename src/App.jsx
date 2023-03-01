7056aca40e94dd16ea822251e2104e963fec873d
ReynaCansecoHernandez <l19350287@tuxtepec.tecnm.mx>
2023-03-01 17:39:15 -0600

    Inicializar el Proyecto-Lista de Hobbies

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+function App() {
+  const nombre = 'Reyna Canseco Hernández'
+
+  return (
+      <div>
+        <h3>Lista de Hobbies {nombre}</h3>
+        <ol>
+          <li>Leer</li>
+          <li>Ver Series y Películas</li>
+          <li>Escuchar Música</li>
+          <li>Practicar Lettering</li>
+          <li>Pasar Tiempo con mis Amigos</li>
+          <li>Cultivar Plantas</li>
+          <li>Aprender Repostería</li>
+          <li>Bordar</li>
+          <li>Hacer Manualidades</li>
+          <li>Fotografía</li>
+        </ol>
+    </div>
+  )
+}
+
+export default App
